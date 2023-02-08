2666aef416e49636b5e866dd1e0174b1858d2781
Vincent <vincent@3wmedia.nl>
2023-02-08 13:45:51 +0100

    darkmode + color settings

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header>
+    <q-header :class="$q.dark.mode ? 'bg-dark' : 'bg-teal'">
       <q-toolbar>
         <q-btn
           flat
@@ -11,25 +11,17 @@
           @click="toggleLeftDrawer"
         />
 
-        <q-toolbar-title>
-         Prosuco
-        </q-toolbar-title>
+        <q-toolbar-title> Prosuco SEO </q-toolbar-title>
 
-
+        <div class="toggleScreenMode">
+          <q-btn flat square round icon="dark_mode" @click="$q.dark.toggle()" />
+        </div>
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
       <q-list>
-        <q-item-label
-          header
-        >
-          Menu
-        </q-item-label>
+        <q-item-label header> Menu </q-item-label>
 
         <EssentialLink
           v-for="link in essentialLinks"
@@ -41,42 +33,44 @@
 
     <q-page-container>
       <q-page class="q-pa-md">
-      <router-view />
-    </q-page>
+        <router-view />
+      </q-page>
     </q-page-container>
   </q-layout>
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
+import { defineComponent, ref } from "vue";
+import EssentialLink from "components/EssentialLink.vue";
 
 const linksList = [
   {
-    title: 'Backlink checker',
-    caption: 'controleer backlinks',
-    icon: 'link',
-    link: {name:'backlinks'}
-  }
-]
+    title: "Backlink checker",
+    caption: "controleer backlinks",
+    icon: "link",
+    link: { name: "backlinks" },
+  },
+];
 
 export default defineComponent({
-  name: 'MainLayout',
+  name: "MainLayout",
 
   components: {
-    EssentialLink
+    EssentialLink,
   },
 
-  setup () {
-    const leftDrawerOpen = ref(false)
+  setup() {
+    const leftDrawerOpen = ref(false);
 
     return {
       essentialLinks: linksList,
       leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
-    }
-  }
-})
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+    };
+  },
+});
 </script>
+
+<style lang="scss"></style>
